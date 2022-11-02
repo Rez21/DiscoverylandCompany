a3b080e693cb5f99d7435b125acd58a0d6a85ab7
Prasad <banprasad21@gmail.com>
2022-11-02 22:44:56 +0530

    [Raghav] Added basic pages of discovery app

---
+++ JS/home.js
@@ -0,0 +1,82 @@
+document.getElementById("head-one").innerHTML = `<div class="logo">
+<a href="../HTML/home.html"><img src="../ASSETS/logo.png" width="26%"></a>
+</div>
+<ul class="head-nav-menu">
+<h3 class="head-nav-item">
+    <a href="../PAGES/aboutDLC.html">About DLC</a>
+</h3>
+<h3 class="head-nav-item">
+    <a href="../PAGES/ourWorld.html">Our Worlds</a>
+</h3>
+<h3 class="head-nav-item">
+    <a href="../PAGES/experience.html">Experiences</a>
+</h3>
+<h3 class="head-nav-item">
+    <a href="../PAGES/gallery.html">Gallery</a>
+</h3>
+<h3 class="head-nav-item">
+    <a href="../HTML/press.html">Press</a>
+</h3>
+</ul>`;
+
+document.getElementById("foot-one").innerHTML = `  <div class="footer-part1">
+<div class="footer-logo">
+    <a href="../HTML/home.html"><img src="../ASSETS/logo-footer.png" width="30%" height="30%"></a>
+</div>
+<div class="footer-context">
+    <p>© Discovery Land Company. All rights reserved.</p>
+</div>
+</div>
+
+<div class="footer-part2">
+<div class="footer-part-one">
+    <h3 class="foot-nav-item1">
+        GET IN TOUCH
+    </h3>
+    <h3 class="foot-nav-item-two">
+        <a href="tel:(480) 624-5200">(480) 624-5200</a>
+    </h3>
+    <h3 class="foot-nav-item-three">
+        <a href="mailto:mail@discoverylandco.com">mail@discoverylandco.com</a>
+    </h3>
+</div>
+<ul class="foot-nav-menu1">
+
+    <h3 class="foot-nav-item">
+        <a href="../PAGES/aboutDLC.html">About DLC</a>
+    </h3>
+    <h3 class="foot-nav-item">
+        <a href="../PAGES/ourWorlds.html">Our Worlds</a>
+    </h3>
+    <h3 class="foot-nav-item">
+        <a href="../PAGES/experience.html">Experiences</a>
+    </h3>
+    <h3 class="foot-nav-item">
+        <a href="../PAGES/gallery.html">Gallery</a>
+    </h3>
+    <h3 class="foot-nav-item">
+        <a href="../PAGES/press.html">Press</a>
+    </h3>
+</ul>
+<ul class="foot-nav-menu2">
+
+    <h3 class="foot-nav-item">
+        <a href="../HTML/magazine.html">Magazine</a>
+    </h3>
+    <h3 class="foot-nav-item">
+        <a href="https://discoverylandco.com/career">Careers</a>
+    </h3>
+    <h3 class="foot-nav-item">
+        <a href="../PAGES/foundation.html">Foundation</a>
+    </h3>
+    <h3 class="foot-nav-item">
+        <a href="../PAGES/company.html">Comapny</a>
+    </h3>
+    <h3 class="foot-nav-item">
+        <a href="https://discoverylandco.com/contact">Contact</a>
+    </h3>
+    <h3 class="foot-nav-item">
+        <a href="">Privacy</a>
+    </h3>
+</ul>
+</div>`;
